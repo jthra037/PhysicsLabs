14c6df7dccb7b562d2727ab7111f95e985a369fb
C401IT35\Tech <its_thrasher@live.ca>
2017-11-28 09:14:20 -0500

    Add movement to pillars

---
+++ Lab2/Assets/Scripts/Lab4/PillarController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarController : MonoBehaviour {
+
+    [Range(4, 9)]
+    [SerializeField] private int sizeMin;
+
+    [Range(1, 5)]
+    [SerializeField] private int speedMax;
+
+    [SerializeField] private int speed;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        transform.localScale = new Vector3(1, Random.Range(sizeMin, 10), 1);
+        speed = Random.Range(1, speed);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(0, 0, -speed);
+    }
+}
