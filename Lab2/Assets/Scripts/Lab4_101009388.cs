f574f8f638d93edddac30cd4f8b29d1a2e333fa9
C401IT35\Tech <its_thrasher@live.ca>
2017-11-28 09:17:54 -0500

    Add ground checking for jump

--- Lab2/Assets/Scripts/Lab4_101009388.cs
+++ Lab2/Assets/Scripts/Lab4_101009388.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class Lab4_101009388 : MonoBehaviour {
 
+    private bool isOnGround;
+    private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            isOnGround = false;
+        }
+    }
 }
