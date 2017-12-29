7d6e9cee1d0b7784994d95a0a7d589ef5d04a576
Jacob-Laptop <its_thrasher@live.ca>
2017-12-29 10:23:00 -0500

    Add a bunch of stuff while travelling and forget about it by the time I have internet again

--- PhysicsFinal/Assets/Scripts/PlayerController.cs
+++ PhysicsFinal/Assets/Scripts/PlayerController.cs
@@ -19,13 +19,17 @@ public class PlayerController : MonoBehaviour {
 
     private float jumpSpeed;
 
+    public bool isShotFromCannon = false;
+    public bool isInCannon = false;
+
 	// Use this for initialization
 	void Start ()
     {
         rb = GetComponent<Rigidbody>();
         rb.maxAngularVelocity = (1.5f * Mathf.PI); // Make sure we don't spin too fast
 
-        jumpSpeed = FindReqJumpSpeed(2.6f); // Figure out what Viy should be to jump 2.6 units all the time
+        //jumpSpeed = FindReqJumpSpeed(2.6f); // Figure out what Viy should be to jump 2.6 units all the time
+        jumpSpeed = FIN.FindViForPeak(2.6f);
 	}
 
 
@@ -50,7 +54,7 @@ public class PlayerController : MonoBehaviour {
         }
 
         // keep track of when the player is falling
-        falling = rb.velocity.y < 0 && Mathf.Abs(rb.velocity.y) > Mathf.Epsilon;
+        falling = rb.velocity.y < 0 && Mathf.Abs(rb.velocity.y) > Mathf.Epsilon && !isShotFromCannon;
     }
 
     // Update is called once per frame
@@ -59,25 +63,35 @@ public class PlayerController : MonoBehaviour {
         // figure out if we should be hampering the players controls because they are in the air
         float airDamp = grounded ? 1 : airbourneModifier;
 
-        // apply the forces by multiplying relevant modifier and input axis
-        rb.AddForce(transform.forward * moveSpeed * airDamp * Input.GetAxisRaw("Vertical"));
-        rb.AddForce(transform.right * moveSpeed * airDamp * Input.GetAxisRaw("Strafe"));
-        rb.AddTorque(transform.up * rotSpeed * airDamp * Input.GetAxis("Horizontal"));
+        if (!(isInCannon || isShotFromCannon))
+        { 
+            // apply the forces by multiplying relevant modifier and input axis
+            rb.AddForce(transform.forward * moveSpeed * airDamp * Input.GetAxisRaw("Vertical"));
+            rb.AddForce(transform.right * moveSpeed * airDamp * Input.GetAxisRaw("Strafe"));
+            rb.AddTorque(transform.up * rotSpeed * airDamp * Input.GetAxis("Horizontal"));
+        }
 
         // only allow player to jump if they are 
         //1) on the ground 
         //2) pressing the jump button 
         //3) if they are not still holding the jump button from a previous jump
-        if(grounded && 
+        if (grounded && 
             Input.GetAxis("Jump") != 0 &&
-            !jumping)
+            !jumping &&
+            !isInCannon)
         {
             jumping = true; // player is now holding the jump button
             rb.AddForce(transform.up * jumpSpeed, ForceMode.VelocityChange); //add jump Viy we found in Start()
         }
+        else if (Input.GetAxis("Jump") != 0 &&
+            isInCannon)
+        {
+            
+        }
 
         // looks less floaty if gravity is more intense during falls
-        if (falling)
+        // this makes projectiles way harder to calculate though, so turn it off sometimes
+        if (falling && !isShotFromCannon)
         {
             rb.AddForce(2 * Physics.gravity);
         }
@@ -89,6 +103,7 @@ public class PlayerController : MonoBehaviour {
         if (other.CompareTag("Ground"))
         {
             grounded = true;
+            isShotFromCannon = false;
         }
     }
 
